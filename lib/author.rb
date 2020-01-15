ae456022f818c7d767a688816642d3257390d2c2
Scott Matsuda <smatsuda2014@gmail.com>
2020-01-15 19:52:57 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,19 @@
+class Author
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+  
+  def add_post(post)
+    post.author = self
+  end
+  
+  def add_post_by_title(post)
+    post = Post.new(post)
+    
+  end
+  
+end
